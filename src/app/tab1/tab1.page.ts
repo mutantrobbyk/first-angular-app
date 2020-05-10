5bc8b170b44c598d73328e016193becb5502397b
mutantrobbyk <robertknowles02@msn.com>
2020-05-10 00:07:55 -0600

    added in edit functionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,55 +1,58 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { TodoService, ITodoList } from './todo.service';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { TodoService, ITodoList } from "./todo.service";
 
 enum ValList {
   ONE,
-  TWO
+  TWO,
 }
 
-type OptionList = 'one' | 'two';
+type OptionList = "one" | "two";
 
 interface B {
-  time: number
+  time: number;
 }
 
 interface Contact extends CommonRecord {
-  first_name: string
+  first_name: string;
 }
 
 interface Job extends CommonRecord {
-  name: string
+  name: string;
 }
 
 interface CommonRecord {
-  id: string
-  type: string
-  date_created: string
+  id: string;
+  type: string;
+  date_created: string;
 }
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page implements OnInit {
-  TodoList: ITodoList
-  @ViewChild('box', {static: false})
-  input: ElementRef
+  TodoList: ITodoList;
+  @ViewChild("box", { static: false })
+  input: ElementRef;
 
   constructor(public todo: TodoService) {}
   ngOnInit(): void {
-   this.TodoList = this.todo.getTodoList()
+    this.TodoList = this.todo.getTodoList();
   }
-  addTodo(name: string): void{
-    this.todo.addTodo(name)
-    this.input.nativeElement.value = ''
+  addTodo(name: string): void {
+    this.todo.addTodo(name);
+    this.input.nativeElement.value = "";
   }
   // bind remove function
-  removeTodo(index: number):void{
-    this.todo.removeTodo(index)
+  removeTodo(index: number): void {
+    this.todo.removeTodo(index);
   }
-  test<T extends B>(a: number, b: T):T{
-    return b
+  editTodo(event: {target: {value: string}}, index: number): void {
+    this.todo.editTodo(event.target.value, index);
+  }
+  test<T extends B>(a: number, b: T): T {
+    return b;
   }
 
   handleChange<T extends CommonRecord>(v: T): T {
@@ -57,21 +60,15 @@ export class Tab1Page implements OnInit {
   }
 
   reactToThing(j: Job, c: Contact) {
-    this.handleChange<Contact>(c)
-    this.handleChange<Job>(j)
-    this.pickOption('id')
+    this.handleChange<Contact>(c);
+    this.handleChange<Job>(j);
+    this.pickOption("id");
   }
 
-  pickOption(contactField: keyof Contact) {
-
-  }
+  pickOption(contactField: keyof Contact) {}
 
   enumTest(v: ValList) {
-    if(v === ValList.ONE) {
-
+    if (v === ValList.ONE) {
     }
   }
-
-  
-
 }
--- src/app/tab1/todo.service.ts
+++ src/app/tab1/todo.service.ts
@@ -7,14 +7,23 @@ export interface ITodoList extends Array<ITodo> {
 }
 @Injectable({providedIn: 'root'})
 export class TodoService {
+    
     private todoList: ITodoList = [{name: 'Laundry'}]
     addTodo(name: string){
         this.todoList.push({name})
     }
-    removeTodo(index){
+    removeTodo(index: number): void{
         this.todoList.splice(index, 1)
     }
     getTodoList(): ITodoList{
         return this.todoList
     }
+    editTodo(value: string, index: number): void{
+        this.todoList[index] = {name: value}
+    }
+    // makeCall(val1, val2){
+    //     axios.post('/api/login', {username: val1, password: val2}).then(result => {
+    //         //do something
+    //     })
+    // }
 }
